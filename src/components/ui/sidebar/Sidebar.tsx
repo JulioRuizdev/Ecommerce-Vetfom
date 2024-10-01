53f90f6080b43b3208f4aca4f6f6a60526dcedcf
JulioKrack <julio.ruiz.dev@gmail.com>
2024-10-01 18:05:22 -0500

    iconos en el sidebar

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { IoCloseOutline, IoSearchOutline } from "react-icons/io5"
+import Link from "next/link";
+import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5"
 
 
 export const Sidebar = () => {
@@ -27,6 +28,43 @@ export const Sidebar = () => {
                     className="w-full bg-gray-50 rounded pl-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500"
                 />
             </div>
+
+            <Link href="/"
+                className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+            >
+                <IoPersonOutline size={30} />
+                <span className="ml-3 text-xl">Perfil</span>
+            </Link>
+
+            <Link href="/"
+                className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+            >
+                <IoTicketOutline size={30} />
+                <span className="ml-3 text-xl">Ordenes</span>
+            </Link>
+
+            <Link href="/"
+                className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+            >
+                <IoLogInOutline size={30} />
+                <span className="ml-3 text-xl">Ingresar</span>
+            </Link>   
+
+            <Link href="/"
+                className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+            >
+                <IoLogOutOutline size={30} />
+                <span className="ml-3 text-xl">Salir</span>
+            </Link>     
+
+            <div className="w-full h-px bg-gray-400 my-10"/>      
+
+            <Link href="/"
+                className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+            >
+                <IoShirtOutline size={30} />
+                <span className="ml-3 text-xl">Productos</span>
+            </Link>                
 
 
         </nav>
