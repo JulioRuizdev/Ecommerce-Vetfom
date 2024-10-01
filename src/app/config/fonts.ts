a05aa0402028e030534023beb2292c5d2faeda12
JulioKrack <julio.ruiz.dev@gmail.com>
2024-09-30 23:18:58 -0500

    se añadio la fuente de titulo

---
+++ src/app/config/fonts.ts
@@ -0,0 +1,7 @@
+import { Montserrat_Alternates } from 'next/font/google';
+
+
+export const titleFont = Montserrat_Alternates({
+    subsets: ['latin'],
+    weight: ['500', '700'],
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+
 import "./globals.css";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -12,6 +14,8 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
