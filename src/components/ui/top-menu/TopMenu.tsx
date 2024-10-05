434080b34ac72464f3f0d40bf3dab88c1c47d289
JulioKrack <julio.ruiz.dev@gmail.com>
2024-10-05 14:18:43 -0500

    carrito terminado

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -44,12 +44,15 @@ export const TopMenu = () => {
                     <IoSearchOutline className='w-5 h-5'></IoSearchOutline>
                 </Link>
 
-                <Link href='/cart' className='mx-2'>
+                <Link href={
+                    ((totalItemsInCart === 0) && loaded) ? '/empty' :
+                    '/cart'
+                    } className='mx-2'>
                     <div className='relative'>
                         {
                             ( loaded && totalItemsInCart > 0) && (
                         
-                            <span className='absolute text-xs px-1 rounded-full font-bold -top-2 -right-2 bg-blue-700 text-white'>
+                            <span className=' fade-in absolute text-xs px-1 rounded-full font-bold -top-2 -right-2 bg-blue-700 text-white'>
                                 {totalItemsInCart}
                             </span>
                         )}
