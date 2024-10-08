ba4c78d327955e354ad8f0970927f01755ccdc41
JulioKrack <julio.ruiz.dev@gmail.com>
2024-10-08 01:44:14 -0500

    ya registra usuarios en la bd

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -6,3 +6,4 @@ export * from './product/get-stock-by-slug';
 
 export * from './auth/login';
 export * from './auth/logout';
+export * from './auth/register';
--- src/app/auth/new-account/ui/RegisterForm.tsx
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -1,7 +1,10 @@
 'use client';
+import { registerUser } from "@/actions";
 import clsx from "clsx";
 import Link from "next/link";
+import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { set } from "zod";
 
 type FormInputs = {
     name: string;
@@ -12,21 +15,28 @@ type FormInputs = {
 
 export const RegisterForm = () => {
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     const {register, handleSubmit, formState: {errors}} = useForm<FormInputs>();
 
     const onSubmit: SubmitHandler<FormInputs> = async(data) => {
+        setErrorMessage('');
+
         const {name, email, password} = data
-        console.log(name, email, password)
+        const resp=await registerUser(name, email, password)
+    
+        if( !resp.ok ){
+            setErrorMessage(resp.message);
+            return;
+        }
+    
+    
     }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
 
-        {
-            // errors.name?.type === 'required' && (
-            //     <div className="text-red-500">El nombre es obligatorio</div>
-            // )
-        }
+
 
     <label htmlFor="email">Nombre Completo </label>
       <input
@@ -71,6 +81,8 @@ export const RegisterForm = () => {
         type="password" 
         {...register("password", {required: true, minLength: 6})}
         />
+
+        <span className="text-red-500">{errorMessage}</span>
 
       <button
         
