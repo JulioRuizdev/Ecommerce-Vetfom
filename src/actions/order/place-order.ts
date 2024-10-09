83e0d088206f91aaf38cb1323f6a4116e9c4a9df
JulioKrack <julio.ruiz.dev@gmail.com>
2024-10-09 16:48:54 -0500

    colocar orden subtotal tax total

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth.config";
 import type { Address } from "@/interfaces";
+import prisma from "@/lib/prisma";
 import { ok } from "assert";
 
 
@@ -22,5 +23,34 @@ export const placeOrder = async( productIds: ProductToOrder[], address: Address)
         }
     }
 
+    //obtener info de los productos
 
+    const products = await prisma.product.findMany({
+        where: {
+            id: {
+                in: productIds.map( p => p.productId)
+            }
+        }
+    });
+
+
+    // calcular montos
+    const itemsInOrder = productIds.reduce( (count, p ) => count + p.quantity, 0 );
+
+
+    // los totales, subtotal, tax y  total
+    const { subTotal, tax, total} = productIds.reduce( (totals, item) => {
+
+        const productQuantity = item.quantity;
+        const product = products.find ( product => product.id === item.productId);
+        if (!product) throw new Error(`${item.productId} no existe - 500`)
+        
+        const subTotal = product.price * productQuantity;
+
+        totals.subTotal += subTotal;
+        totals.tax += subTotal * 0.18;
+        totals.total += subTotal * 1.18;    
+
+        return totals;
+    }, {subTotal: 0, tax: 0, total: 0});
 }
