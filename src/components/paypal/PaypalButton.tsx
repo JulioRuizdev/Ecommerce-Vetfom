1f53f06e73915b7945ff89b77d59fb393b502390
JulioKrack <julio.ruiz.dev@gmail.com>
2024-10-10 16:25:12 -0500

    skeleton en botones paypal

--- src/components/paypal/PaypalButton.tsx
+++ src/components/paypal/PaypalButton.tsx
@@ -1,7 +1,20 @@
 'use client';
-import { PayPalButtons } from "@paypal/react-paypal-js"
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js"
 
 export const PaypalButton = () => {
+
+    const [{ isPending }] = usePayPalScriptReducer();
+
+    if( isPending){
+        return (
+            <div className="animate-pulse mb-16">
+                <div className="h-11 bg-gray-300 rounded" />
+                <div className="h-11 bg-gray-300 rounded mt-2" />
+            </div>
+
+        )
+    }
+    
   return (
     
     <PayPalButtons />
